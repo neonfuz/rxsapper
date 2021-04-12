4e3be161d380f5bff6e23d87090fe4faf453601f
Sage Raflik <neonfuz@gmail.com>
2021-04-11 19:51:55 -0500

    counters: local state component

---
+++ src/components/Counters.svelte
@@ -0,0 +1,46 @@
+<script>
+ let counters = [
+     { name: 'Foo', value: 2 },
+     { name: 'Bar', value: 5 },
+     { name: 'Baz', value: 12 },
+ ];
+ const addCounter = () => counters = [...counters, { name: '', value: 0 }];
+ const removeCounter = i => () => counters = [...counters.slice(0,i), ...counters.slice(i+1)];
+</script>
+
+
+<table>
+    <thead>
+        <tr>
+            <th>Score</th>
+            <th>Name</th>
+            <th>Delete</th>
+        </tr>
+    </thead>
+    <tbody>
+        {#each counters as {name, value}, i}
+            <tr>
+                <td><input type=number bind:value /></td>
+                <td><input type=text bind:value={name} /></td>
+                <td><button on:click={removeCounter(i)}>🗑</button></td>
+            </tr>
+        {/each}
+        <tr>
+            <td colspan=3>
+                <button style=width:100% on:click={addCounter}>+</button>
+            </td>
+        </tr>
+    </tbody>
+
+</table>
+
+
+
+<style>
+ input {
+     text-align: center;
+ }
+ input[type=number] {
+     width: 3.2em;
+ }
+</style>
--- src/routes/counters.svelte
+++ src/routes/counters.svelte
@@ -1,7 +1,11 @@
+<script>
+ import Counters from "../components/Counters.svelte";
+</script>
+
 <svelte:head>
     <title>Counters</title>
 </svelte:head>
 
 <h1>Counters</h1>
 
-<p>This is the 'counters' page. There's not much here.</p>
+<Counters />
